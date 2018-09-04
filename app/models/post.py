682041cbeb73488e344283f9d00179c76fc8604f
GennadiiTurutin <gennadii.turutin@gmail.com>
2018-09-04 19:28:08 -0400

    Preparation for deployment

--- app/models/post.py
+++ app/models/post.py
@@ -2,49 +2,13 @@ from app import db, login
 from datetime import datetime
 from app.models import User
 
-class SearchableMixin(object):
-    @classmethod
-    def search(cls, expression, page, per_page):
-        ids, total = query_index(cls.__tablename__, expression)
-        if total == 0:
-            return cls.query.filter_by(id=0), 0
-        when = []
-        for i in range(len(ids)):
-            when.append((ids[i], i))
-        return cls.query.filter(cls.id.in_(ids)).order_by(
-            db.case(when, value=cls.id)), total
-
-    @classmethod
-    def before_commit(cls, session):
-        session._changes = {
-            'add': [obj for obj in session.new if isinstance(obj, cls)], 
-            'update': [obj for obj in session.dirty if isinstance(obj, cls)],
-            'delete': [obj for obj in session.deleted if isinstance(obj, cls)]
-        }
-
-    @classmethod
-    def after_commit(cls, session):
-        for obj in session._changes['add']:
-            add_to_index(cls.__tablename__, obj)
-        for obj in session._changes['update']:
-            add_to_index(cls.__tablename__, obj)
-        for obj in session._changes['delete']:
-            remove_from_index(cls.__tablename__, obj)
-        session._changes = None
-
-    @classmethod
-    def reindex(cls):
-        for obj in cls.query:
-            add_to_index(cls.__tablename__, obj)
-
-
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 
-class Post(SearchableMixin, db.Model):
+class Post(db.Model):
     __tablename__ = 'posts'
     __searchable__ = ['title', 'subtitle', 'content']
     id = db.Column(db.Integer, primary_key=True)
@@ -60,9 +24,7 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post %r>' % self.title
-
-db.event.listen(db.session, 'before_commit', Post.before_commit)
-db.event.listen(db.session, 'after_commit', Post.after_commit)       
+    
 
 
 
