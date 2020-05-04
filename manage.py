2db557b8275d38f3fbec94bb150f1fb33bb0c63c
GennadiiTurutin <gennadii.turutin@gmail.com>
2020-05-03 20:10:55 -0400

    Refactoring

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from app import create_app, db
-# from gevent.pywsgi import WSGIServer
 from waitress import serve
 
 app = create_app('default')
@@ -10,9 +9,4 @@ app = create_app('default')
 # manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    # manager.run()
-    print('Starting')
-    #app.run(host='0.0.0.0')
     serve(app, host="0.0.0.0", port=5000)
-    # http_server = WSGIServer(('', 5000), app)
-    # http_server.serve_forever()
