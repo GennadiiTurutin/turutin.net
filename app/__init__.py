1e9f2e5b153c085e3445cd846e9d9b0284a6e4d6
GennadiiTurutin <gennadii.turutin@gmail.com>
2020-05-03 21:01:48 -0400

    Updates

--- app/__init__.py
+++ app/__init__.py
@@ -33,10 +33,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    db.init_app(app)
     login.init_app(app)
     mail.init_app(app)
-    bootstrap = Bootstrap(app)
     admin = Admin(app, template_mode='bootstrap3', index_view=MyAdminIndexView())
     sess.init_app(app)
    
--- config.py
+++ config.py
@@ -2,20 +2,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 from decouple import config
 
-import redis
-
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY')
+    SECRET_KEY                     = config('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_POOL_RECYCLE = 299
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = config('FLASKY_ADMIN')
-    SSL_REDIRECT = False
-    MAIL_HOST_USER = config('MAIL_HOST_USER')
-    MAIL_HOST_PASSWORD = config('MAIL_HOST_PASSWORD')
-    MAIL_HOST_URL = config('MAIL_HOST_URL')
+    SQLALCHEMY_POOL_RECYCLE        = 299
+    FLASKY_MAIL_SUBJECT_PREFIX     = '[Flasky]'
+    FLASKY_MAIL_SENDER             = 'Gennadii Admin <gennadii.turutin@gmail.com>'
+    FLASKY_ADMIN                   = config('FLASKY_ADMIN')
+    SSL_REDIRECT                   = False
+    MAIL_HOST_USER                 = config('MAIL_HOST_USER')
+    MAIL_HOST_PASSWORD             = config('MAIL_HOST_PASSWORD')
+    MAIL_HOST_URL                  = config('MAIL_HOST_URL')
 
     # Flask-Session
     SESSION_TYPE = 'filesystem'
@@ -36,12 +34,6 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="GennadiiT",
-    password=os.environ.get('database_password'),
-    hostname="GennadiiT.mysql.pythonanywhere-services.com",
-    databasename="GennadiiT$MyBlog",
-    )
 
     @classmethod
     def init_app(cls, app):
