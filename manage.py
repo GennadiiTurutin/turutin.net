46e58dcda8193c5256edbd805d2739d3841341cd
GennadiiTurutin <gennadii.turutin@gmail.com>
2020-05-03 21:09:17 -0400

    Update

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from app import create_app, db
 from waitress import serve
 
 app = create_app('default')
-
+# manager = Manager(app)
+# migrate = Migrate(app, db)
+# manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=5000)
